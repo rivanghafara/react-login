673c3937f61573ee79f4786225c8731e7b9104e2
rivanghafara <rgdevrianza@gmail.com>
2022-06-12 11:47:32 +0700

    add auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,53 @@
-import Signup from './Signup'
-import Login from './Login'
-import ForgotPassword from './ForgotPassword'
-import UpdateProfile from './UpdateProfile'
-import AddItem from './AddItem'
-import MenuItem from './MenuItem'
-import EditItem from './EditItem'
-import Dashboard from './Dashboard'
-import Navbars from './Navbars'
-import { Container } from 'react-bootstrap'
-import { AuthProvider } from '../contexts/AuthContext'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import PrivateRoute from './PrivateRoute'
-import MyProfile from './MyProfile'
+import Signup from "./Signup";
+import Login from "./Login";
+import ForgotPassword from "./ForgotPassword";
+import UpdateProfile from "./UpdateProfile";
+import AddItem from "./AddItem";
+import MenuItem from "./MenuItem";
+import EditItem from "./EditItem";
+import Dashboard from "./Dashboard";
+import Navbars from "./Navbars";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+import MyProfile from "./MyProfile";
+import { Redirect } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 
 function App() {
+  const { currentUser } = useAuth();
+
   return (
-    <Container className="flex justify-content-center" style={{ minHeight: "100vh" }}>
-      <div className="w-100" >
+    <Container
+      className="flex justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100">
         <Router>
-          <AuthProvider>
-            <Navbars />
-            <Switch>
-              <PrivateRoute exact path='/' component={Dashboard} />
-              <PrivateRoute path='/update-profile' component={UpdateProfile} />
-              <PrivateRoute path='/create-item' component={AddItem} />
-              <PrivateRoute path='/edit-item/:id' component={EditItem} />
-              <PrivateRoute exact path='/manage/menu-item' component={MenuItem} />
-              <PrivateRoute exact path='/manage/my-profile' component={MyProfile} />
-              <Route path='/signup' component={Signup} />
-              <Route path='/login' component={Login} />
-              <Route path='/forgot-password' component={ForgotPassword} />
-            </Switch>
-          </AuthProvider>
+          <Navbars />
+          <Switch>
+            <PrivateRoute exact path="/" component={Dashboard} />
+            <PrivateRoute path="/update-profile" component={UpdateProfile} />
+            <PrivateRoute path="/create-item" component={AddItem} />
+            <PrivateRoute path="/edit-item/:id" component={EditItem} />
+            <PrivateRoute exact path="/manage/menu-item" component={MenuItem} />
+            <PrivateRoute
+              exact
+              path="/manage/my-profile"
+              component={MyProfile}
+            />
+            <Route exact path="/signup">
+              {currentUser ? <Redirect to="/" /> : <Signup />}
+            </Route>
+            <Route exact path="/login">
+              {currentUser ? <Redirect to="/" /> : <Login />}
+            </Route>
+            <Route path="/forgot-password" component={ForgotPassword} />
+          </Switch>
         </Router>
       </div>
     </Container>
-  )
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import "bootstrap/dist/css/bootstrap.min.css"
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { AuthProvider } from "./contexts/AuthContext";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <AuthProvider>
+    <App />
+  </AuthProvider>,
+  document.getElementById("root")
 );
